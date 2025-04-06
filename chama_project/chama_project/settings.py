a0692090f2d5bb811b03501c37ae70e34e40a07b
Davyduty <129392472+Davyduty@users.noreply.github.com>
2025-04-06 06:48:51 +0300

    Update settings.py

--- chama_project/chama_project/settings.py
+++ chama_project/chama_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://chama-project-django.onrender.com']
+ALLOWED_HOSTS = [ALLOWED_HOSTS = ['chama-project-django.onrender.com', 'localhost', '127.0.0.1']]
 
 # Application definition
 INSTALLED_APPS = [
